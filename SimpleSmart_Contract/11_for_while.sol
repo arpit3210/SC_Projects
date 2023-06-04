09ffee44e5d2e70f485658ca3f9d25e29b4696ab
1234 <arpitsinghthakur321@gmail.com>
2023-06-04 23:51:09 +0530

    use of for loops in solidity

---
+++ SimpleSmart_Contract/11_for_while.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GBL-3.0
+
+pragma solidity >=0.8.0 <0.9.0;
+
+
+contract forwhiles{
+function loops() external pure{
+
+    for(uint i=0; i<10; i++)
+    {
+if(i==3)
+{
+    continue;
+}
+
+//more code
+
+    }
+}
+
+}
