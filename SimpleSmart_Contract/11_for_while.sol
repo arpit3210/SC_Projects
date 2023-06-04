e46b3bfb720ec057cd040d95735228e19f88cc93
1234 <arpitsinghthakur321@gmail.com>
2023-06-04 23:53:24 +0530

    use of for loops solidity

--- SimpleSmart_Contract/11_for_while.sol
+++ SimpleSmart_Contract/11_for_while.sol
@@ -2,20 +2,14 @@
 
 pragma solidity >=0.8.0 <0.9.0;
 
+contract forwhiles {
+    function loops() external pure {
+        for (uint i = 0; i < 10; i++) {
+            if (i == 3) {
+                continue;
+            }
 
-contract forwhiles{
-function loops() external pure{
-
-    for(uint i=0; i<10; i++)
-    {
-if(i==3)
-{
-    continue;
-}
-
-//more code
-
+            //more code
+        }
     }
-}
-
 }
