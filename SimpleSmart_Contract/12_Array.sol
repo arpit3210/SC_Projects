c9a55c06f1fbe49f3f259c7c45dc016e5319ad24
1234 <arpitsinghthakur321@gmail.com>
2023-06-05 00:00:24 +0530

    use of for loops sidity

---
+++ SimpleSmart_Contract/12_Array.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GBL-3.0
+
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Arrays {
+    
+uint[] public nums=[1,2,3,23,345,5,65,657,234,678,5,567];
+
+uint[3] public numsFixed=[4,5,6];
+
+
+    function PushArrayElemnetsNums()  public 
+    {
+nums.push(76542364);
+    }
+    
+
+    
+    
+    
+    
+    /// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+/// @return Documents the return variables of a contract’s function state variable
+/// @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name){
+
+
+
+}
